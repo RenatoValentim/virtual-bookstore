971b5103f523f715fc3b4a4bd35c143997b8516c
RenatoValentim <rvrenato28@gmail.com>
2024-01-23 20:40:04 -0300

    refactor: format the description text

--- internal/usecases/register_author_test.go
+++ internal/usecases/register_author_test.go
@@ -131,7 +131,14 @@ func TestRegisterAuthor(t *testing.T) {
 	})
 
 	t.Run(`Should not register a new author if don't have a description field`, func(t *testing.T) {
-		desc := "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
+		desc := `Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore
+		culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat
+		excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+		voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia.
+		Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem
+		duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+		ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
+		sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.`
 		input := db_contracts.Author{
 			Name:        "John Doe",
 			Email:       "johndoe@example.com",
